663e655b097238531688229c04ae25dd5b7b014c
Mandeep <maxmangat@gmail.com>
2022-04-07 17:24:59 -0500

    bugfix

--- app/src/main/java/com/example/c196/UI/DetailedTermActivity.java
+++ app/src/main/java/com/example/c196/UI/DetailedTermActivity.java
@@ -2,8 +2,10 @@ package com.example.c196.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.c196.Database.Repository;
@@ -20,6 +22,9 @@ public class DetailedTermActivity extends AppCompatActivity {
     int termID;
     Repository repo;
 
+    Button delete;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +40,13 @@ public class DetailedTermActivity extends AppCompatActivity {
         termStarting.setText(start);
         termEnding.setText(end);
 
+        delete = findViewById(R.id.termDelete);
+
         repo = new Repository(getApplication());
+
+        if (termID == 0){
+            delete.setText("Cancel");
+        }
 
 
 
@@ -43,8 +54,12 @@ public class DetailedTermActivity extends AppCompatActivity {
 
     public void saveButton(View view) {
         Terms term;
+        int   newID = 1;
         if (termID == 0){
-            int newID = repo.getAllTerms().size() + 1;
+            if (repo.getAllTerms().size()>0){
+                newID = repo.getAllTerms().get(repo.getAllTerms().size()-1).getTermID()+1;
+            }
+
             term = new Terms(newID,termName.getText().toString(), termStarting.getText().toString(),termEnding.getText().toString());
             repo.insert(term);
         }
@@ -52,5 +67,18 @@ public class DetailedTermActivity extends AppCompatActivity {
             term = new Terms(termID,termName.getText().toString(), termStarting.getText().toString(),termEnding.getText().toString());
             repo.update(term);
         }
+
+        Intent intent = new Intent(DetailedTermActivity.this, TermActivity.class);
+        startActivity(intent);
+    }
+
+    public void deleteButton(View view) {
+        Terms term;
+        if (termID !=0){
+        term = new Terms(termID,termName.getText().toString(), termStarting.getText().toString(),termEnding.getText().toString());
+        repo.delete(term);}
+        Intent intent = new Intent(DetailedTermActivity.this, TermActivity.class);
+        startActivity(intent);
+
     }
 }
